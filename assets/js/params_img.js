414a1d948c84ed88cefd2e1e0877022f8b54c688
espinosa98 <cristhian.espinosa01@unicatolica.edu.co>
2023-12-14 17:46:26 -0500

    se agregan nuevas referencias

--- assets/js/params_img.js
+++ assets/js/params_img.js
@@ -10,37 +10,37 @@ export const imagenes = [
     { principal: `${base_modal}ref_05/DELANTERONEGRO5.webp`, interna: `${base_modal}ref_05/ESPALDANEGRO5.webp`, precio: `60.000`, referencia: `OVERSIZED "PREMIUM" LLAMAS DEL BERSERKER NEGRO`,
         imagen_carrousel: `${base_modal}ref_05/DELANTERONEGRO5.webp`, imagen_carrousel_2: `${base_modal}ref_05/ESPALDANEGRO5.webp` },
 
-    { principal: `${base_modal}ref_01/FRENTEAZUL1.webp`, interna: `${base_modal}ref_01/ESPALDAAZUL1.webp`, precio: `55.000`, referencia: `OVERSIZED "PREMIUM" BASICA WOLFEIN AZUL`,
+    { principal: `${base_modal}ref_01/ESPALDAAZUL1.webp`, interna: `${base_modal}ref_01/FRENTEAZUL1.webp`, precio: `55.000`, referencia: `OVERSIZED "PREMIUM" BASICA WOLFEIN AZUL`,
         imagen_carrousel: `${base_modal}ref_01/FRENTEAZUL1.webp`, imagen_carrousel_2: `${base_modal}ref_01/ESPALDAAZUL1.webp` },
 
-    { principal: `${base_modal}ref_01/FRENTEGRIS1.webp`, interna: `${base_modal}ref_01/ESPALDAGRIS1.webp`, precio: `55.000`, referencia: `OVERSIZED "PREMIUM" BASICA WOLFEIN GRIS`,
+    { principal: `${base_modal}ref_01/ESPALDAGRIS1.webp`, interna: `${base_modal}ref_01/FRENTEGRIS1.webp`, precio: `55.000`, referencia: `OVERSIZED "PREMIUM" BASICA WOLFEIN GRIS`,
         imagen_carrousel: `${base_modal}ref_01/FRENTEGRIS1.webp`, imagen_carrousel_2: `${base_modal}ref_01/ESPALDAGRIS1.webp` },
 
-    { principal: `${base_modal}ref_01/FRENTENEGRO1.webp`, interna: `${base_modal}ref_01/ESPALDANEGRO1.webp`, precio: `55.000`, referencia: `OVERSIZED "PREMIUM" BASICA WOLFEIN NEGRO`,
+    { principal: `${base_modal}ref_01/ESPALDANEGRO1.webp`, interna: `${base_modal}ref_01/FRENTENEGRO1.webp`, precio: `55.000`, referencia: `OVERSIZED "PREMIUM" BASICA WOLFEIN NEGRO`,
         imagen_carrousel: `${base_modal}ref_01/FRENTENEGRO1.webp`, imagen_carrousel_2: `${base_modal}ref_01/ESPALDANEGRO1.webp` },
 
-    { principal: `${base_modal}ref_04/DELANTEROGRIS4.webp`, interna: `${base_modal}ref_04/ESPALDAGRIS4.webp`, precio: `70.000`, referencia: `OVERSIZED "PREMIUM" TORMENTA NOCTURNA GRIS`,
+    { principal: `${base_modal}ref_04/ESPALDAGRIS4.webp`, interna: `${base_modal}ref_04/DELANTEROGRIS4.webp`, precio: `70.000`, referencia: `OVERSIZED "PREMIUM" TORMENTA NOCTURNA GRIS`,
         imagen_carrousel: `${base_modal}ref_04/DELANTEROGRIS4.webp`, imagen_carrousel_2: `${base_modal}ref_04/ESPALDAGRIS4.webp` },
 
-    { principal: `${base_modal}ref_04/DELANTERONEGRO4.webp`, interna: `${base_modal}ref_04/ESPALDANEGRO4.webp`, precio: `70.000`, referencia: `OVERSIZED "PREMIUM" TORMENTA NOCTURNA NEGRO`,
+    { principal: `${base_modal}ref_04/ESPALDANEGRO4.webp`, interna: `${base_modal}ref_04/DELANTERONEGRO4.webp`, precio: `70.000`, referencia: `OVERSIZED "PREMIUM" TORMENTA NOCTURNA NEGRO`,
         imagen_carrousel: `${base_modal}ref_04/DELANTERONEGRO4.webp`, imagen_carrousel_2: `${base_modal}ref_04/ESPALDANEGRO4.webp` },
 
-    { principal: `${base_modal}ref_02/DELANTEROAZUL2.webp`, interna: `${base_modal}ref_02/ESPALDAAZUL2.webp`, precio: `60.000`, referencia: `OVERSIZED "PREMIUM" FURIA NOCTURNA AZUL`,
+    { principal: `${base_modal}ref_02/ESPALDAAZUL2.webp`, interna: `${base_modal}ref_02/DELANTEROAZUL2.webp`, precio: `60.000`, referencia: `OVERSIZED "PREMIUM" FURIA NOCTURNA AZUL`,
         imagen_carrousel: `${base_modal}ref_02/DELANTEROAZUL2.webp`, imagen_carrousel_2: `${base_modal}ref_02/ESPALDAAZUL2.webp` },
 
-    { principal: `${base_modal}ref_02/DELANTEROGRIS2.webp`, interna: `${base_modal}ref_02/ESPALDAGRIS2.webp`, precio: `60.000`, referencia: `OVERSIZED "PREMIUM" FURIA NOCTURNA GRIS`,
+    { principal: `${base_modal}ref_02/ESPALDAGRIS2.webp`, interna: `${base_modal}ref_02/DELANTEROGRIS2.webp`, precio: `60.000`, referencia: `OVERSIZED "PREMIUM" FURIA NOCTURNA GRIS`,
         imagen_carrousel: `${base_modal}ref_02/DELANTEROGRIS2.webp`, imagen_carrousel_2: `${base_modal}ref_02/ESPALDAGRIS2.webp` },
 
-    { principal: `${base_modal}ref_02/DELANTERONEGRO2.webp`, interna: `${base_modal}ref_02/ESPALDANEGRA2.webp`, precio: `60.000`, referencia: `OVERSIZED "PREMIUM" FURIA NOCTURNA NEGRO`,
+    { principal: `${base_modal}ref_02/ESPALDANEGRA2.webp`, interna: `${base_modal}ref_02/DELANTERONEGRO2.webp`, precio: `60.000`, referencia: `OVERSIZED "PREMIUM" FURIA NOCTURNA NEGRO`,
         imagen_carrousel: `${base_modal}ref_02/DELANTERONEGRO2.webp`, imagen_carrousel_2: `${base_modal}ref_02/ESPALDANEGRA2.webp` },
 
-    { principal: `${base_modal}ref_03/DELANTEROAZUL3.webp`, interna: `${base_modal}ref_03/ESPALDAAZUL3.webp`, precio: `65.000`, referencia: `OVERSIZED "PREMIUM" BERSERKER AZUL`,
+    { principal: `${base_modal}ref_03/ESPALDAAZUL3.webp`, interna: `${base_modal}ref_03/DELANTEROAZUL3.webp`, precio: `65.000`, referencia: `OVERSIZED "PREMIUM" BERSERKER AZUL`,
         imagen_carrousel: `${base_modal}ref_03/DELANTEROAZUL3.webp`, imagen_carrousel_2: `${base_modal}ref_03/ESPALDAAZUL3.webp` },
 
-    { principal: `${base_modal}ref_03/DELANTEROGRIS3.webp`, interna: `${base_modal}ref_03/ESPALDAGRIS3.webp`, precio: `65.000`, referencia: `OVERSIZED "PREMIUM" BERSERKER GRIS`,
+    { principal: `${base_modal}ref_03/ESPALDAGRIS3.webp`, interna: `${base_modal}ref_03/DELANTEROGRIS3.webp`, precio: `65.000`, referencia: `OVERSIZED "PREMIUM" BERSERKER GRIS`,
         imagen_carrousel: `${base_modal}ref_03/DELANTEROGRIS3.webp`, imagen_carrousel_2: `${base_modal}ref_03/ESPALDAGRIS3.webp` },
 
-    { principal: `${base_modal}ref_03/DELANTERONEGRO3.webp`, interna: `${base_modal}ref_03/ESPALDANEGRO3.webp`, precio: `65.000`, referencia: `OVERSIZED "PREMIUM" BERSERKER NEGRO`,
+    { principal: `${base_modal}ref_03/ESPALDANEGRO3.webp`, interna: `${base_modal}ref_03/DELANTERONEGRO3.webp`, precio: `65.000`, referencia: `OVERSIZED "PREMIUM" BERSERKER NEGRO`,
         imagen_carrousel: `${base_modal}ref_03/DELANTERONEGRO3.webp`, imagen_carrousel_2: `${base_modal}ref_03/ESPALDANEGRA3.webp` },
 
     { principal: `${base_modal}joggers/joggerbeige1.webp`, interna: `${base_modal}joggers/joggerbeige1.webp`, precio: `60.000`, referencia: `JOGGERS BEIGE`,
